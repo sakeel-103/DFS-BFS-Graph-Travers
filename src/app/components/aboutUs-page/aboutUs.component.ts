4cdb4b454c818d37c0db989c407bec061c03c196
Rudra Pratap Singh <130541940+rudrapratap63@users.noreply.github.com>
2024-10-10 00:30:29 +0530

    Add about us page and fix navbar

---
+++ src/app/components/aboutUs-page/aboutUs.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+@Component({
+  selector: 'app-aboutUs-page',
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './aboutUs.component.html',
+  styleUrls: ['./aboutUs.component.css'],
+})
+export class AboutUsComponent {}
