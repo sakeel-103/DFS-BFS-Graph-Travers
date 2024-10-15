b78f97f2b61320c7e78543d07e86e007a49bf7bf
Jashwanth-rit <158207370+Jashwanth-rit@users.noreply.github.com>
2024-10-16 00:30:37 +0530

    fixes:219

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { AboutUsComponent } from './components/aboutUs-page/aboutUs.component';
 import { LoginPageComponent } from './components/login-page/login-page.component';
 import { MainIndexComponent } from './components/main-index/main-index.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -15,9 +18,11 @@ import { NavbarComponent } from './components/navbar/navbar.component';
     LoginPageComponent,
     MainIndexComponent,
     NavbarComponent,
+    
+    RouterModule,
   ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
+  styleUrls: ['./app.component.css'], // Corrected here
 })
 export class AppComponent {
   title = 'Graph-Traversal';
