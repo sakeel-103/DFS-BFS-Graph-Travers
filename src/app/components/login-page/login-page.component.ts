df086a106837b36500c22d12bbab3f1cf255e4b4
Jashwanth-rit <158207370+Jashwanth-rit@users.noreply.github.com>
2024-10-18 19:33:59 +0530

    jashwanth/LoginPage

--- src/app/components/login-page/login-page.component.ts
+++ src/app/components/login-page/login-page.component.ts
@@ -1,50 +1,55 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NgForm, FormsModule } from '@angular/forms';
-import { CommonModule } from '@angular/common'; // Import CommonModule
+import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+
 
 @Component({
   selector: 'app-login-page',
   standalone: true,
-  imports: [RouterModule, FormsModule, CommonModule], // Add CommonModule here
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.css'],
+  imports: [CommonModule,FormsModule,RouterModule]
 })
 export class LoginPageComponent {
   passwordVisible: boolean = false;
+  loginData = { username: '', password: '' };
 
   constructor(private router: Router, private toastr: ToastrService) {}
 
   login(form: NgForm): void {
+    if (form.invalid) {
+      this.toastr.error('Please fill in valid credentials.', 'Validation Error');
+      return;
+    }
+
     const { username, password } = form.value;
     const isSuccess = this.authenticate(username, password);
-    console.log('username', username, 'pass', password);
+
     if (isSuccess) {
       this.toastr.success('Login Successfully', 'Success');
-      this.router.navigate(['/mainIndex']); // Uncomment this line to navigate upon successful login
+      this.router.navigate(['/mainIndex']);
     } else {
-      console.error('Login failed');
       this.toastr.error('Invalid username or password', 'Error');
     }
   }
 
   private authenticate(username: string, password: string): boolean {
-    return username === 'test' && password === 'password';
-  }
-
-  goBack() {
-    this.router.navigate(['/signup']); // Navigate back to the signup page
+    return username === 'test' && password === 'Password123!';
   }
 
   togglePasswordVisibility() {
-    this.passwordVisible = !this.passwordVisible; // Toggle the password visibility
+    this.passwordVisible = !this.passwordVisible;
+  }
+
+  goBack() {
+    this.router.navigate(['/signup']);
   }
 
   loginWithGoogle(): void {
-    // Google authentication in future.
-    //added to get rid of error : NG9: Property 'loginWithGoogle' does not exist on type 'LoginPageComponent'.
     console.log('Google login initiated');
     this.toastr.success('Google login successful', 'Success');
   }
