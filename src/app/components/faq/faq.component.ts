aa61e1b9f45dd5e047152eda69f989aa75b6e856
Sourabh782 <sourabhsinghrawat782@gmail.com>
2024-10-20 16:51:19 +0530

    enhanced FAQ page

--- src/app/components/faq/faq.component.ts
+++ src/app/components/faq/faq.component.ts
@@ -10,22 +10,39 @@ import { NavbarComponent } from '../navbar/navbar.component';
 })
 export class FaqComponent implements AfterViewInit {
   ngAfterViewInit() {
-    const faqItems = document.querySelectorAll<HTMLElement>('.faq-item');
-    faqItems.forEach((item) => {
-      const question = item.querySelector('.faq-question');
-      if (question) {
-        question.addEventListener('click', () => {
-          item.classList.toggle('open');
-          const answer = item.querySelector<HTMLElement>('.faq-answer');
-          if (answer) {
-            if (item.classList.contains('open')) {
-              answer.style.maxHeight = `${answer.scrollHeight}px`;
+    // const faqItems = document.querySelectorAll<HTMLElement>('.faq-item');
+    // faqItems.forEach((item) => {
+    //   const question = item.querySelector('.faq-question');
+    //   if (question) {
+    //     question.addEventListener('click', () => {
+    //       item.classList.toggle('open');
+    //       const answer = item.querySelector<HTMLElement>('.faq-answer');
+    //       if (answer) {
+    //         if (item.classList.contains('open')) {
+    //           answer.style.maxHeight = `${answer.scrollHeight}px`;
+    //         } else {
+    //           answer.style.maxHeight = '0';
+    //         }
+    //       }
+    //     });
+    //   }
+    // });
+    document.addEventListener('DOMContentLoaded', () => {
+      const togglers = document.querySelectorAll('[data-toggle]');
+      
+        togglers.forEach((btn) => {
+          btn.addEventListener('click', (e:any) => {
+             const selector = e.currentTarget?.dataset.toggle
+             const block = document.querySelector(`${selector}`) as HTMLElement;
+            if (e.currentTarget?.classList.contains('active')) {
+              block.style.maxHeight = '';
             } else {
-              answer.style.maxHeight = '0';
+              block.style.maxHeight = block.scrollHeight + 'px';
             }
-          }
-        });
-      }
-    });
+              
+             e.currentTarget?.classList.toggle('active')
+          })
+        })
+      })
   }
 }
