07c8e3872510da8ec849e5ac5850fbbff63f5fde
Riya Chauhan <96919050+Riyachauhan11@users.noreply.github.com>
2024-10-20 06:19:44 +0530

    ensures all visualizations display

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -5,33 +5,32 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule,  CommonModule],
+  imports: [RouterModule, CommonModule],
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent implements AfterViewInit, OnDestroy {
   private resizeListener: (() => void) | null = null; // Initialize as null
   activeTab: string = 'dfs'; // Default tab is 'dfs'
-  showGif: boolean = false;
-  // Function to set the active tab
+  showBFSGif: boolean = false; // Default for BFS visualization
+  showDFSGif: boolean = false; // Default for DFS visualization
+  showDijkstraGif: boolean = false; // Default for Dijkstra visualization
+
   setActiveTab(tab: string) {
     this.activeTab = tab;
   }
+
   ngAfterViewInit() {
     const openNav = document.getElementById('menu-icon');
     const navLinks = document.getElementById('nav-links');
 
     if (openNav) {
       openNav.onclick = () => {
-        openNav.classList.toggle("active-menu");
+        openNav.classList.toggle('active-menu');
         if (navLinks) {
-          navLinks.classList.toggle("active-navbar");
-        } else {
-          console.error('Nav links not found');
+          navLinks.classList.toggle('active-navbar');
         }
       };
-    } else {
-      console.error('Menu icon not found');
     }
 
     // Add resize event listener
@@ -39,7 +38,6 @@ export class HeaderComponent implements AfterViewInit, OnDestroy {
     window.addEventListener('resize', this.resizeListener);
   }
 
-  // Function to handle the resize event
   private handleResize() {
     const navLinks = document.getElementById('nav-links');
     if (window.innerWidth > 886 && navLinks) {
@@ -48,13 +46,23 @@ export class HeaderComponent implements AfterViewInit, OnDestroy {
   }
 
   ngOnDestroy() {
-    // Clean up the event listener only if it was set
     if (this.resizeListener) {
       window.removeEventListener('resize', this.resizeListener);
     }
   }
+
   startBFS() {
-    this.showGif = !this.showGif; // Toggle the visibility
-    console.log("BFS Started: ", this.showGif); // Debugging
+    this.showBFSGif = !this.showBFSGif; // Toggle the visibility
+    console.log('BFS Started: ', this.showBFSGif); // Debugging
+  }
+
+  startDFS() {
+    this.showDFSGif = !this.showDFSGif; // Toggle the visibility
+    console.log('DFS Started: ', this.showDFSGif); // Debugging
+  }
+
+  startDijkstra() {
+    this.showDijkstraGif = !this.showDijkstraGif; // Toggle the visibility
+    console.log('Dijkstra Started: ', this.showDijkstraGif); // Debugging
   }
 }
